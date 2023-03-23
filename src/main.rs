83f2300f52796e008e531f8d9e53006333f4be77
Gabe Jensen <thegabejensen@gmail.com>
2023-03-23 07:16:58 -0700

    Made two endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
 #[macro_use]
 extern crate rocket;
-
 mod cors;
 use cors::CORS;
-use rocket::http::Header;
-use rocket::{Request, Response};
+
+// #[derive(Serialize, FromRow)]
+// struct Todo {
+//     pub id: i32,
+//     pub note: String,
+// }
+
+// #[derive(Deserialize)]
+// struct TodoNew {
+//     pub note: String,
+// }
+
+// struct MyState {
+//     pool: PgPool,
+// }
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, there 5"
+    "It's working!"
+}
+
+#[get("/hello")]
+fn hello() -> &'static str {
+    "Hello world!"
 }
 
 #[shuttle_runtime::main]
 async fn rocket() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/hello", routes![index]).attach(CORS);
+    let rocket = rocket::build()
+        .mount("/", routes![index, hello])
+        .attach(CORS);
 
     Ok(rocket.into())
 }
