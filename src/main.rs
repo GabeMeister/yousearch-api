eab615895f3182615925d3eb58bc64a8076c870a
Gabe Jensen <thegabejensen@gmail.com>
2023-03-23 07:10:55 -0700

    Moved cors setup into another file

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,10 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::fairing::{Fairing, Info, Kind};
+mod cors;
+use cors::CORS;
 use rocket::http::Header;
 use rocket::{Request, Response};
-
-pub struct CORS;
-
-#[rocket::async_trait]
-impl Fairing for CORS {
-    fn info(&self) -> Info {
-        Info {
-            name: "Add CORS headers to responses",
-            kind: Kind::Response,
-        }
-    }
-
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new(
-            "Access-Control-Allow-Methods",
-            "POST, GET, PATCH, OPTIONS",
-        ));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-    }
-}
 
 #[get("/")]
 fn index() -> &'static str {
