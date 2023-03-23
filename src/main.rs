dbd8d9485784cd4440add9c95cc374d61eec1d0f
Gabe Jensen <thegabejensen@gmail.com>
2023-03-23 07:44:24 -0700

    Got some more requests in to add and retrieve todos

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,26 @@
 extern crate rocket;
 mod cors;
 use cors::CORS;
+use rocket::serde::json::Json;
+use rocket::{response::status::BadRequest, State};
+use serde::{Deserialize, Serialize};
+use shuttle_runtime::CustomError;
+use sqlx::{Executor, FromRow, PgPool};
 
-// #[derive(Serialize, FromRow)]
-// struct Todo {
-//     pub id: i32,
-//     pub note: String,
-// }
+#[derive(Serialize, FromRow)]
+struct Todo {
+    pub id: i32,
+    pub note: String,
+}
 
-// #[derive(Deserialize)]
-// struct TodoNew {
-//     pub note: String,
-// }
+#[derive(Deserialize)]
+struct TodoNew {
+    pub note: String,
+}
 
-// struct MyState {
-//     pool: PgPool,
-// }
+struct MyState {
+    pool: PgPool,
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -28,10 +33,42 @@ fn hello() -> &'static str {
     "Hello world!"
 }
 
+#[get("/todo/<id>")]
+async fn retrieve(id: i32, state: &State<MyState>) -> Result<Json<Todo>, BadRequest<String>> {
+    let todo: Todo = sqlx::query_as("select * from todos where id = $1")
+        .bind(id)
+        .fetch_one(&state.pool)
+        .await
+        .map_err(|e| BadRequest(Some(e.to_string())))?;
+
+    Ok(Json(todo))
+}
+
+#[post("/todo", data = "<data>")]
+async fn add(
+    data: Json<TodoNew>,
+    state: &State<MyState>,
+) -> Result<Json<Todo>, BadRequest<String>> {
+    let todo: Todo = sqlx::query_as("insert into todos (note) values ($1) returning id, note")
+        .bind(&data.note)
+        .fetch_one(&state.pool)
+        .await
+        .map_err(|e| BadRequest(Some(e.to_string())))?;
+
+    Ok(Json(todo))
+}
+
 #[shuttle_runtime::main]
-async fn rocket() -> shuttle_rocket::ShuttleRocket {
+async fn rocket(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_rocket::ShuttleRocket {
+    pool.execute(include_str!("../schema.sql"))
+        .await
+        .map_err(CustomError::new)?;
+
+    let state = MyState { pool };
+
     let rocket = rocket::build()
-        .mount("/", routes![index, hello])
+        .mount("/", routes![index, hello, add, retrieve])
+        .manage(state)
         .attach(CORS);
 
     Ok(rocket.into())
